c4705911fd08222b8c1ab7a924558543fa03a5a4
dynasty919 <dynasty919@163.com>
2018-02-16 04:14:28 +0800

    Median Maintenance

--- course2_pa3_MedianMaintenance.py
+++ course2_pa3_MedianMaintenance.py
@@ -4,6 +4,8 @@
 # @Email   : dynasty919@163.com
 # @File    : course2_pa2_dijkstra.py
 # @Software: PyCharm
+import sys
+import threading
 
 def readfile():
     with open('Median.txt', 'rt') as f:
@@ -13,50 +15,156 @@ def readfile():
             b.append(int(iter1.strip()))
         return b
 
-heapmin = []
-heapmax = []
-global ismaxlonger
-
-def init_ismaxlonger():
-    global ismaxlonger
-    ismaxlonger = False
-
-def invert_ismaxlonger():
-    global ismaxlonger
-    ismaxlonger = not ismaxlonger
-
-def heapmin_insert(xint):
+def heapmin_insert(heapmin, xint):
+    def pop_up(heapmin, n):
+        if n == 1:
+            return heapmin
+        else:
+            if n % 2:
+                if heapmin[n - 1] > heapmin[n // 2 - 1]:
+                    return heapmin
+                else:
+                    heapmin[n - 1], heapmin[n // 2 - 1] = heapmin[n // 2 - 1], heapmin[n - 1]
+                    return pop_up(heapmin, n // 2)
+            else:
+                if heapmin[n - 1] > heapmin[(n - 1) // 2]:
+                    return heapmin
+                else:
+                    heapmin[n - 1], heapmin[(n - 1) // 2] = heapmin[(n - 1) // 2], heapmin[n - 1]
+                    return pop_up(heapmin, (n + 1) // 2)
     if not heapmin:
         heapmin.append(xint)
+        return heapmin
     else:
         heapmin.append(xint)
-        pop_up(heapmin.index(xint))
+        heapmin = pop_up(heapmin, len(heapmin))
+        return heapmin
 
-
-def two_heaps(xint):
-    invert_ismaxlonger()
-    if ismaxlonger:
-        if xint > heapmax[0]:
-            heapmin_insert(xint)
+def heapmax_insert(heapmax, xint):
+    def pop_up(heapmax, n):
+        if n == 1:
+            return heapmax
         else:
-            heapmin_insert(heap_extract[0])
-            heapmax_extract()
-            heapmax_insert(xint)
+            if n % 2:
+                if heapmax[n - 1] < heapmax[n // 2 - 1]:
+                    return heapmax
+                else:
+                    heapmax[n - 1], heapmax[n // 2 - 1] = heapmax[n // 2 - 1], heapmax[n - 1]
+                    return pop_up(heapmax, n // 2)
+            else:
+                if heapmax[n - 1] < heapmax[(n - 1) // 2]:
+                    return heapmax
+                else:
+                    heapmax[n - 1], heapmax[(n - 1) // 2] = heapmax[(n - 1) // 2], heapmax[n - 1]
+                    return pop_up(heapmax, (n + 1) // 2)
+    if not heapmax:
+        heapmax.append(xint)
+        return heapmax
     else:
-        if xint > heapmin[0]:
-            heapmax_insert(heapmin[0])
-            heapmin_extract()
-            heapmin_insert(xint)
+        heapmax.append(xint)
+        heapmax = pop_up(heapmax, len(heapmax))
+        return heapmax
+
+def heapmin_extract(heapmin):
+    def pop_down(heapmin, n):
+        if 2*n > len(heapmin):
+            return heapmin
+        elif 2*n == len(heapmin):
+            if heapmin[n-1] > heapmin[2*n-1]:
+                heapmin[n-1], heapmin[2*n-1] = heapmin[2*n-1], heapmin[n-1]
+                return pop_down(heapmin, 2*n)
+            else:
+                return heapmin
         else:
-            heapmax_insert(xint)
-    return heapmax[0]
+            if heapmin[n-1] < heapmin[2*n-1] and heapmin[n-1] < heapmin[2*n]:
+                return heapmin
+            elif heapmin[n-1] > heapmin[2*n-1] and heapmin[n-1] < heapmin[2*n]:
+                heapmin[n - 1], heapmin[2 * n - 1] = heapmin[2 * n - 1], heapmin[n - 1]
+                return pop_down(heapmin, 2 * n)
+            elif heapmin[n-1] > heapmin[2*n] and heapmin[n-1] < heapmin[2*n-1]:
+                heapmin[n - 1], heapmin[2 * n] = heapmin[2 * n], heapmin[n - 1]
+                return pop_down(heapmin, 2*n+1)
+            else:
+                if heapmin[2*n-1] < heapmin[2*n]:
+                    heapmin[n - 1], heapmin[2 * n - 1] = heapmin[2 * n - 1], heapmin[n - 1]
+                    return pop_down(heapmin, 2 * n)
+                else:
+                    heapmin[n - 1], heapmin[2 * n] = heapmin[2 * n], heapmin[n - 1]
+                    return pop_down(heapmin, 2 * n + 1)
+    heapmin[0] = heapmin[-1]
+    heapmin.pop()
+    heapmin = pop_down(heapmin, 1)
+    return heapmin
 
+def heapmax_extract(heapmax):
+    def pop_down(heapmax, n):
+        if 2*n > len(heapmax):
+            return heapmax
+        elif 2*n == len(heapmax):
+            if heapmax[n-1] < heapmax[2*n-1]:
+                heapmax[n-1], heapmax[2*n-1] = heapmax[2*n-1], heapmax[n-1]
+                return pop_down(heapmax, 2*n)
+            else:
+                return heapmax
+        else:
+            if heapmax[n-1] > heapmax[2*n-1] and heapmax[n-1] > heapmax[2*n]:
+                return heapmax
+            elif heapmax[n-1] < heapmax[2*n-1] and heapmax[n-1] > heapmax[2*n]:
+                heapmax[n - 1], heapmax[2 * n - 1] = heapmax[2 * n - 1], heapmax[n - 1]
+                return pop_down(heapmax, 2 * n)
+            elif heapmax[n-1] < heapmax[2*n] and heapmax[n-1] > heapmax[2*n-1]:
+                heapmax[n - 1], heapmax[2 * n] = heapmax[2 * n], heapmax[n - 1]
+                return pop_down(heapmax, 2*n+1)
+            else:
+                if heapmax[2*n-1] > heapmax[2*n]:
+                    heapmax[n - 1], heapmax[2 * n - 1] = heapmax[2 * n - 1], heapmax[n - 1]
+                    return pop_down(heapmax, 2 * n)
+                else:
+                    heapmax[n - 1], heapmax[2 * n] = heapmax[2 * n], heapmax[n - 1]
+                    return pop_down(heapmax, 2 * n + 1)
+    heapmax[0] = heapmax[-1]
+    heapmax.pop()
+    heapmax = pop_down(heapmax, 1)
+    return heapmax
 
+def two_heaps(heapmax, heapmin, xint):
+    if heapmax == []:
+        heapmax.append(xint)
+        return (heapmax, heapmin, xint)
+    else:
+        if len(heapmax) == len(heapmin):
+            if xint < heapmin[0]:
+                heapmax = heapmax_insert(heapmax, xint)
+                return (heapmax, heapmin, heapmax[0])
+            else:
+                heapmax = heapmax_insert(heapmax, heapmin[0])
+                heapmin = heapmin_extract(heapmin)
+                heapmin = heapmin_insert(heapmin, xint)
+                return (heapmax, heapmin, heapmax[0])
+        elif len(heapmax) > len(heapmin):
+            if xint > heapmax[0]:
+                heapmin = heapmin_insert(heapmin, xint)
+                return (heapmax, heapmin, heapmax[0])
+            else:
+                heapmin = heapmin_insert(heapmin, heapmax[0])
+                heapmax = heapmax_extract(heapmax)
+                heapmax = heapmax_insert(heapmax, xint)
+                return (heapmax, heapmin, heapmax[0])
+        else:
+            print('error')
 
 def median_heap():
     stream = readfile()
-    result1 = []
-    init_ismaxlonger()
+    result = []
+    heapmin = []
+    heapmax = []
     for iter2 in stream:
-        result1.append(two_heaps(iter2))
-    return sum(result1)%10000
+        (heapmax, heapmin, mid) = two_heaps(heapmax, heapmin, iter2)
+        result.append(mid)
+    print(result)
+    return sum(result)
+
+def main():
+    print(median_heap())
+
+main()
