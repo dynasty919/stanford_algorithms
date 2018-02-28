00a58f62090c7621d34de9f80dda7f77c531dd05
dynasty919 <dynasty919@163.com>
2018-03-01 00:35:39 +0800

    clustring

---
+++ course3_pa2_clustering1.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/28 22:30
+# @Author  : dynasty919
+# @Email   : dynasty919@163.com
+# @File    : clustering.py
+# @Software: PyCharm
+import functools
+
+def readfile():
+    with open('clustering1.txt','rt') as f:
+        a = f.readlines()
+        b = []
+        for c in a[1:]:
+            b.append(c.strip().split(' '))
+        d ={}
+        for e in b:
+            d[(e[0], e[1])] = int(e[2])
+        return d
+
+def greedy(verteces, distances):
+    for dist in distances:
+        if verteces[dist[0]] != verteces[dist[1]]:
+            init = dist
+            break
+    return functools.reduce(lambda x, y: y if verteces[y[0]] != verteces[y[1]] and distances[y] < distances[x] else x, distances, init)
+
+def clustering(distances):
+    verteces = {}
+    for iter in range(500):
+        verteces[str(iter+1)] = iter + 1
+    while len(set(verteces.values())) > 4:
+        (vertex1, vertex2) = greedy(verteces, distances)
+        temp = verteces[vertex2]
+        for vertex in verteces:
+            if verteces[vertex] == temp:
+                verteces[vertex] = verteces[vertex1]
+    return verteces
+
+def main():
+    distances = readfile()
+    verteces = clustering(distances)
+    print(distances)
+    print(verteces)
+    result = {}
+    for v in verteces:
+        if verteces[v] in result:
+            result[verteces[v]].append(v)
+        else:
+            result[verteces[v]] = [v]
+    print(result)
+    maxdis = greedy(verteces, distances)
+    print(maxdis)
+    print(distances[maxdis])
+
+main()
